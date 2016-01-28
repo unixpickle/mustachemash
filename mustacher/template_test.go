6eba9b5d437e7571df2692d141667bb55897dec8
Alex Nichol <unixpickle@gmail.com>
2016-01-27 20:32:36 -0500

    test and benchmark Template.Correlations()
    This will help me ensure that I don't break the Template implementation as I optimize it.

--- mustacher/template_test.go
+++ mustacher/template_test.go
@@ -27,8 +27,41 @@ func TestMaxCorrelation(t *testing.T) {
 		}
 	}
 	positiveImg, err := readAssetImage("positive.jpg")
+	if err != nil {
+		t.Fatal(err)
+	}
 	if corr := template.MaxCorrelation(positiveImg); math.Abs(corr-1) > 0.001 {
 		t.Error("bad correlation", corr, "for positive")
+	}
+}
+
+func TestCorrelations(t *testing.T) {
+	templateImg, err := readAssetImage("template.png")
+	if err != nil {
+		t.Fatal(err)
+	}
+	template := NewTemplate(templateImg)
+	for i := 0; i < 3; i++ {
+		image, err := readAssetImage("negative_" + strconv.Itoa(i) + ".jpg")
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+		matches := template.Correlations(image, 0.98)
+		if len(matches) > 0 {
+			t.Error("got false matches for negative", i)
+		}
+	}
+	positiveImg, err := readAssetImage("positive.jpg")
+	if err != nil {
+		t.Fatal(err)
+	}
+	matches := template.Correlations(positiveImg, 0.98)
+	matches = matches.NonOverlappingSet()
+	if len(matches) != 1 {
+		t.Error("invalid number of matches for positive:", len(matches))
+	} else if matches[0].X != 201 || matches[0].Y != 593 {
+		t.Error("invalid match for positive:", matches[0])
 	}
 }
 
@@ -51,6 +84,22 @@ func BenchmarkMaxCorrelation(b *testing.B) {
 		for _, neg := range negatives {
 			template.MaxCorrelation(neg)
 		}
+	}
+}
+
+func BenchmarkCorrelations(b *testing.B) {
+	templateImg, err := readAssetImage("template.png")
+	if err != nil {
+		b.Fatal(err)
+	}
+	template := NewTemplate(templateImg)
+	positiveImg, err := readAssetImage("positive.jpg")
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		template.Correlations(positiveImg, 0.98)
 	}
 }
 
