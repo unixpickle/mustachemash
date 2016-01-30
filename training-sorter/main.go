b864aad8db8e07b358505e18ec9b5a45ed18593c
Alex Nichol <unixpickle@gmail.com>
2016-01-30 13:11:09 -0500

    created training-sorted utility
    This utility helps me test changes I make to my training samples. Every time I change my training samples, I re-run this program to see if any new images work or if any old ones break.

---
+++ training-sorter/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/unixpickle/mustachemash/mustacher"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		printErrors("Usage: training-sorter <database> <sort dir>",
+			"",
+			"training-sorter runs many images against a template",
+			"database and separates the ones that work from the",
+			"ones that don't.",
+			"",
+			"At first, the sort directory may just contain a bunch of",
+			"images; this will create directories to sort them into.",
+			"",
+			"Image filenames must be of the form X_Y_D.(png|jpeg),",
+			"where X and Y indicate the mustache center and D",
+			"specifies the allowed margin of error.")
+		os.Exit(1)
+	}
+
+	db, err := mustacher.ReadDatabase(os.Args[1])
+	if err != nil {
+		printErrors(err)
+		os.Exit(1)
+	}
+
+	imageFiles, err := findImageFiles(os.Args[2])
+	if err != nil {
+		printErrors(err)
+		os.Exit(1)
+	}
+
+	SortImages(os.Args[2], imageFiles, db)
+}
+
+func printErrors(errors ...interface{}) {
+	for _, err := range errors {
+		fmt.Fprintln(os.Stderr, err)
+	}
+}
+
+func findImageFiles(dir string) ([]string, error) {
+	subdirs := []Category{"", FalsePositive, TruePositive, BothPositive, Negative}
+	res := []string{}
+	for _, subdir := range subdirs {
+		dirPath := filepath.Join(dir, string(subdir))
+		file, err := os.Open(dirPath)
+		if err != nil {
+			if subdir == "" {
+				return nil, err
+			}
+			if err := os.Mkdir(dirPath, 0755); err != nil {
+				return nil, err
+			}
+			file, err = os.Open(dirPath)
+			if err != nil {
+				return nil, err
+			}
+			continue
+		}
+		contents, err := file.Readdirnames(-1)
+		file.Close()
+		if err != nil {
+			return nil, err
+		}
+		for _, name := range contents {
+			categoryName := Category(name)
+			if subdir == "" && (categoryName == FalsePositive || categoryName == TruePositive ||
+				categoryName == BothPositive || categoryName == Negative) {
+				continue
+			} else if strings.HasPrefix(name, ".") {
+				continue
+			}
+			res = append(res, filepath.Join(dirPath, name))
+		}
+	}
+	return res, nil
+}
---
+++ training-sorter/sort.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+	"sync"
+
+	"github.com/unixpickle/mustachemash/mustacher"
+)
+
+func SortImages(basePath string, paths []string, db *mustacher.Database) {
+	log.Println("Sorting...")
+
+	pathChan := make(chan string, len(paths))
+	for _, path := range paths {
+		pathChan <- path
+	}
+	close(pathChan)
+
+	wg := &sync.WaitGroup{}
+	for i := 0; i < runtime.GOMAXPROCS(0); i++ {
+		wg.Add(1)
+		go SortImageRoutine(basePath, db, pathChan, wg)
+	}
+
+	wg.Wait()
+}
+
+func SortImageRoutine(basePath string, db *mustacher.Database, ch <-chan string,
+	wg *sync.WaitGroup) {
+	defer wg.Done()
+	for path := range ch {
+		category, err := CategorizeImage(path, db)
+		if err != nil {
+			log.Println("Error:", err)
+			continue
+		}
+		destPath := filepath.Join(basePath, string(category), filepath.Base(path))
+		if path != destPath {
+			log.Println("Re-categorizing", path, "to", category)
+			if err := os.Rename(path, destPath); err != nil {
+				log.Println("Error:", err)
+			}
+		}
+	}
+}
